dd3da62991e05f76cab9ef89b418892c20f5d0d9
Robert Kemp <robert.kemp04@outlook.com>
2023-11-28 16:35:21 -0600

    Italian Filter complete

--- app.js
+++ app.js
@@ -144,7 +144,15 @@ function findMexicanFood(allDishes) {
 function findItalianFood(allDishes) {
     alert("Searching for Italian dishes...")
     // TODO #2: Filter all dishes for those that have a cuisine type of Italian
+    let results = allDishes.filter(function (el) {
+        if (el.cuisine === "Italian") {
+            return true;
+        } else {
+            return false;
+        }
+    });
     alert("Found all Italian dishes!  Check the console for full output")
+    return results
 }
 
 function searchCuisines(allDishes) {
